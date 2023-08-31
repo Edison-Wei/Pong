b5be8844b7607e0885f45da9aa30198f381aef44
Dreamz2 <Edwei395@gmail.com>
2023-08-31 00:02:54 -0700

    Set up files

---
+++ Pong/Pong_Game/src/Pong/Ball.java
@@ -0,0 +1,16 @@
+package Pong;
+
+public class Ball {
+	int x;
+	int y;
+	int velocity = 0;
+	
+	Ball() { 
+		// Create Ball and place in center of Pane
+	}
+	
+	int sendToRandomDirection() {
+		// Find a way to send the ball in a random direction
+		return 0;
+	}
+}
---
+++ Pong/Pong_Game/src/Pong/Player.java
@@ -0,0 +1,9 @@
+package Pong;
+
+public class Player {
+	
+	Player() {
+		
+	}
+
+}
