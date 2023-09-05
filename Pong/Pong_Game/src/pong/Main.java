438290d9391fa6bd0a00f8add9f39bf2d94c2e05
Dreamz2 <Edwei395@gmail.com>
2023-09-05 01:27:23 -0700

    Need to fix Main class to display the paddles and ball

--- Pong/Pong_Game/src/pong/Main.java
+++ Pong/Pong_Game/src/pong/Main.java
@@ -1,14 +1,24 @@
 package pong;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	Pane rootPane;
+	GameHandler game;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -16,17 +26,69 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Pane rootPane = new Pane();
+		rootPane = new Pane();
+		rootPane.setStyle("-fx-background-color: Black");
 		
-		
+		menuScreen();
 		
 		Scene scene = new Scene(rootPane, 500, 500);
-		scene.setFill(Color.BLACK);
+//		scene.setFill(Color.BLACK);
 		
 		primaryStage.setTitle("Pong");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	// Creates and adds the collection of nodes to the rootPane
+	void menuScreen() {
+		Text title = createText("Pong", 15);
 		
+		Button start = new Button("Start");
+		start.setFont(Font.font("Arial", FontWeight.MEDIUM, FontPosture.REGULAR, 12));
+		start.setTextFill(Color.WHITE);
+		// not show the button background
+		
+		start.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				gameScreen();
+			}
+		});
+		
+		Pane menu = new Pane(title, start);
+		
+		rootPane.getChildren().add(menu);
+	}
+	
+	void gameScreen() {
+		rootPane.getChildren().remove(0);
+		
+		VBox scoreBoard = new VBox(5);
+		
+		Text userText = createText("Player", 12);
+		Text botText = createText("Bot", 12);
+		Text userScore = createText("0", 12);
+		Text botScore = createText("0", 12);
+		
+		HBox labelPane = new HBox(15, userText, botText);
+		HBox scorePane = new HBox(25, userScore, botScore);
+		scoreBoard.getChildren().addAll(labelPane, scorePane);
+		
+		Pane gamePane = new Pane(scoreBoard);
+		
+		rootPane.getChildren().add(gamePane);
+		
+		// create gamehandler
+	}
+	
+	// Returns a Text object with the given String and font size
+	Text createText(String word, int size) {
+		Text newText = new Text(word);
+		newText.setFont(Font.font("Arial", FontWeight.MEDIUM, FontPosture.REGULAR, size));
+		newText.setFill(Color.WHITE);
+		
+		return newText;
 	}
 	
 	// Can add later
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Retro-Games
+# Retro-Game
+Using 3 different programming languages to create Pong, Space Invaders, and Pool.
+With the intention to focus on clean code
